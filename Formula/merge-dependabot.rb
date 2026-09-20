c7d91d59f65de5d402ae2df4cc315b1bab997c76
Siavosh Zarrasvand <siavosh@zarrasvand.com>
2026-09-21 01:34:01 +0300

    chore(release): v1.0.0 formula sha256 update

--- Formula/merge-dependabot.rb
+++ Formula/merge-dependabot.rb
@@ -2,7 +2,7 @@ class MergeDependabot < Formula
   desc "Merge green Dependabot PRs and sync all repos in ~/Documents/Github"
   homepage "https://github.com/SiavoshZarrasvand/homebrew-merge-dependabot"
   url "https://github.com/SiavoshZarrasvand/homebrew-merge-dependabot/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "3de2749a2d02aa83b217d061bab8968fda402b7b2585a6c90bdf4cb977d148f3"
   license "MIT"
   version "1.0.0"
 
